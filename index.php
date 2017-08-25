22221dbc0fae8fbf3b77351a91b96c4009e03777
kairodem <abdallah_3addoula@hotmail.com>
2017-08-25 18:39:55 +0100

    added palette

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 <html>
 <head>
 	<title>YoungCode Project</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/reset.css">
-	<script type="text/javascript" src="assets/scripts/app.js"></script>
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css?<?php echo rand(); ?>">
+	<link rel="stylesheet" type="text/css" href="assets/css/reset.css?<?php echo rand(); ?>">
+	<script type="text/javascript" src="assets/scripts/app.js?<?php echo rand(); ?>"></script>
+	<link href="https://fonts.googleapis.com/css?family=Cairo:200,300,400,600,700,900&amp;subset=arabic" rel="stylesheet">
 </head>
 <body>
 	<div class="header">
 		<div class="wrapper">
-			<div class=""></div>
+		<center><div class="logo"><h1>Mawhibah</h1></div></center>
 		</div>
 	</div>
 </body>
