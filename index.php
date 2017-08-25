583b272b6ca35db49a73d8fe5567038388f47f23
kairodem <abdallah_3addoula@hotmail.com>
2017-08-25 12:42:39 +0100

    added main files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+	<script type="text/javascript" src="assets/scripts/app.js"></script>
+</head>
+<body>
+	<div class="header">
+		<div class="wrapper">
+			<div class=""></div>
+		</div>
+	</div>
+</body>
+</html>
