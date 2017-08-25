fb0837f6708e62d03d8b87d9df4a7862a7e6af83
kairodem <abdallah_3addoula@hotmail.com>
2017-08-25 19:48:40 +0100

    gg

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,4 +3,5 @@ window.onload = function() {
 	var backs = ["w.jpg","w1.jpg","w2.jpg"]
 	var r = Math.floor(Math.random()*3) // generates a number between 0 and 2 inclusive
 	document.body.style.backgroundImage = "url('" + "assets/img/" + backs[r] + "')";
+	document.body.style.animation = "fade 0.5s ease-out";
 }
--- index.php
+++ index.php
@@ -5,13 +5,22 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/main.css?<?php echo rand(); ?>">
 	<link rel="stylesheet" type="text/css" href="assets/css/reset.css?<?php echo rand(); ?>">
 	<script type="text/javascript" src="assets/scripts/app.js?<?php echo rand(); ?>"></script>
-	<link href="https://fonts.googleapis.com/css?family=Cairo:200,300,400,600,700,900&amp;subset=arabic" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
 </head>
 <body>
 	<div class="contentTop">
-		<div class="wrapper">
-			
+		<div class="header">
+			<div class="wrapper">
+				<center><div class="logo"><h1>Snoooooooop</h1></div></center>
+				<div class="nav">
+					<ul>
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+					</ul>
+				</div>
+			</div>
 		</div>
+		
 	</div>
 </body>
 </html>
