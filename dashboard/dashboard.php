1f24c345f770673a44666f97ce81bae120b7a9aa
kairodem <abdallah_3addoula@hotmail.com>
2017-08-26 01:32:59 +0100

    d

---
+++ dashboard/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mawhibah | A social network for talented & skilled people</title>
+	<link rel="stylesheet" type="text/css" href="../assets/css/dash.css?<?php echo rand(); ?>">
+	<link rel="stylesheet" type="text/css" href="../assets/css/reset.css?<?php echo rand(); ?>">
+	<script type="text/javascript" src="../assets/scripts/jquery.js?<?php echo rand(); ?>"></script>
+	<script type="text/javascript" src="../assets/scripts/dash.js?<?php echo rand(); ?>"></script>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+</head>
+<body>
+
+	<div class="profileCover">
+		<div class="overlay">			
+			<div class="wrapper">
+				<center><div class="profilePic"></div></center>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
